99def2bbf62b80f97f2686f8e4da9bce1ff4ce26
k-miyoshi <miyoshiez22@gmail.com>
2021-04-26 23:28:51 +0900

    add types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,19 +1,21 @@
 export interface MeterProps {
-  height?: number;  // メーターの横幅(px)
-  width?: number;   //メーターの縦幅(px)
-  start?: number;   // 目盛りの始まりの角度(deg)
-  end?: number;     //目盛りの終わりの角度(deg)
-  scales?: number;  // 目盛りの数、0があるので実際にはこれより一つ多くなる
-  unitsLabel?: string;   // 単位表示
-  guageInterval?: number;// 目盛りに数値を表示する間隔
-  currentValue: number;//ゲージの現在の値
+  guageHeight?: number;       // メーターの横幅(px)
+  guageWidth?: number;        //メーターの縦幅(px)
+  guageStart?: number;        // 目盛りの始まりの角度(deg)
+  guageEnd?: number;          //目盛りの終わりの角度(deg)
+  guageScales?: number;       // 目盛りの数
+  guageUnits?: string;        // 単位表示
+  guageInterval?: number;     // 目盛りに数値を表示する間隔
+  currentValue: number;       //ゲージの現在の値
+  scaleCoefficient?: number;  //目盛りの係数
+  guageLimit: number;         //メーターの最高値
 }
 
 export type PickType<T, K extends keyof T> = T[K];
 
 export interface StyleProps {
-  'height': string; 
-  'width': string;
+  'guage-height': string; 
+  'guage-width': string;
   'scale-deg': string;
   'offset-deg': string;
   'outline-border': string;
